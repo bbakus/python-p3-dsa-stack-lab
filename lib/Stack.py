83c6c8864882a7ff68d1cd65c0fd09ab2ab6caf0
Brandon Bakus <brandonbakus@gmail.com>
2025-03-04 14:53:36 -0500

    hanging in there

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,41 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+        
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+            return True
+        else:
+            return False 
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        for i in range(len(self.items)):
+            if self.items[i] == target:
+                
+                return len(self.items) - 1 - i
+        return -1
